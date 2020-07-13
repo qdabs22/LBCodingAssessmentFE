63d074629c53f8d18e7a33dc44196d4826fe8594
qdabs22 <qdabs22@gmail.com>
2020-07-12 18:29:21 -0600

    Add all project files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+    }
 }
 
-export default App;
+    componentDidMount() {
+       fetch('https://api.github.com/repos/angular/angular/issues')
+
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+            })
+        });
+    }
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+
+        if(!isLoaded){
+            return <div>Loading...</div>;
+        }
+        else {
+            return (
+            <div>
+            <center><h1>Angular Issues List</h1></center>
+            <center><h6>(created within the last 7 days)</h6></center>
+            {items.filter(item => new Date().getDate() - new Date(item.created_at).getDate() == 4).map((filteredItem) => (
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Issue Name: {filteredItem.title}</h5>
+                    <h6 class="card-subtitle mb-2">Issue User Login: {filteredItem.user.login}</h6>
+                    <h6 class="card-subtitle mb-2">Assignee Login: {filteredItem.assignees.login}</h6>
+                    <p class="card-text">{filteredItem.body}</p>
+                </div>
+                </div>
+            ))}
+            </div> 
+            )
+        }
+    }
+}
+
+export default App; 
